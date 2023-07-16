ecbdb8ea8d100543aab6d28fb5ce49d23b83ff3a
ulascancmgl <ulascancmgl@gmail.com>
2023-07-16 11:10:49 +0300

    northamerican seperation easy normal hard

---
+++ lib/model/flag_capitol.dart
@@ -0,0 +1,248 @@
+// A model that cas country name, country flag, country capitol
+class Country {
+  String name;
+  String flagPath;
+  String capital;
+
+  Country({required this.name, required this.flagPath, required this.capital});
+}
+
+List<Country> hardNorthAmericaCountries = [
+  Country(
+    name: 'Antigua and Barbuda',
+    flagPath: 'assets/flags/antigua_and_barbuda_flag.png',
+    capital: 'Saint John\'s',
+  ),
+  Country(
+    name: 'Belize',
+    flagPath: 'assets/flags/belize_flag.png',
+    capital: 'Belmopan',
+  ),
+  Country(
+    name: 'Grenada',
+    flagPath: 'assets/flags/grenada_flag.png',
+    capital: 'Saint George\'s',
+  ),
+  Country(
+    name: 'Honduras',
+    flagPath: 'assets/flags/honduras_flag.png',
+    capital: 'Tegucigalpa',
+  ),
+  Country(
+    name: 'Nicaragua',
+    flagPath: 'assets/flags/nicaragua_flag.png',
+    capital: 'Managua',
+  ),
+  Country(
+    name: 'Saint Kitts and Nevis',
+    flagPath: 'assets/flags/saint_kitts_and_nevis_flag.png',
+    capital: 'Basseterre',
+  ),
+  Country(
+    name: 'Saint Lucia',
+    flagPath: 'assets/flags/saint_lucia_flag.png',
+    capital: 'Castries',
+  ),
+  Country(
+    name: 'Saint Vincent and the Grenadines',
+    flagPath: 'assets/flags/saint_vincent_and_the_grenadines_flag.png',
+    capital: 'Kingstown',
+  ),
+  Country(
+    name: 'Trinidad and Tobago',
+    flagPath: 'assets/flags/trinidad_and_tobago_flag.png',
+    capital: 'Port of Spain',
+  ),
+];
+List<Country> normalNorthAmericaCountries = [
+  Country(
+    name: 'Bahamas',
+    flagPath: 'assets/flags/bahamas_flag.png',
+    capital: 'Nassau',
+  ),
+  Country(
+    name: 'Barbados',
+    flagPath: 'assets/flags/barbados_flag.png',
+    capital: 'Bridgetown',
+  ),
+  Country(
+    name: 'Dominica',
+    flagPath: 'assets/flags/dominica_flag.png',
+    capital: 'Roseau',
+  ),
+  Country(
+    name: 'Dominican Republic',
+    flagPath: 'assets/flags/dominican_republic_flag.png',
+    capital: 'Santo Domingo',
+  ),
+  Country(
+    name: 'El Salvador',
+    flagPath: 'assets/flags/el_salvador_flag.png',
+    capital: 'San Salvador',
+  ),
+  Country(
+    name: 'Guatemala',
+    flagPath: 'assets/flags/guatemala_flag.png',
+    capital: 'Guatemala City',
+  ),
+  Country(
+    name: 'Jamaica',
+    flagPath: 'assets/flags/jamaica_flag.png',
+    capital: 'Kingston',
+  ),
+  Country(
+    name: 'Panama',
+    flagPath: 'assets/flags/panama_flag.png',
+    capital: 'Panama City',
+  ),
+];
+List<Country> easyNorthAmericaCountries = [
+  Country(
+    name: 'United States of America',
+    flagPath: 'assets/flags/united_states_of_america_flag.png',
+    capital: 'Washington, D.C.',
+  ),
+  Country(
+    name: 'Canada',
+    flagPath: 'assets/flags/canada_flag.png',
+    capital: 'Ottawa',
+  ),
+  Country(
+    name: 'Costa Rica',
+    flagPath: 'assets/flags/costa_rica_flag.png',
+    capital: 'San Jose',
+  ),
+  Country(
+    name: 'Cuba',
+    flagPath: 'assets/flags/cuba_flag.png',
+    capital: 'Havana',
+  ),
+  Country(
+    name: 'Haiti',
+    flagPath: 'assets/flags/haiti_flag.png',
+    capital: 'Port-au-Prince',
+  ),
+  Country(
+    name: 'Mexico',
+    flagPath: 'assets/flags/mexico_flag.png',
+    capital: 'Mexico City',
+  ),
+];
+
+List<Country> northAmericaCountries = [
+  Country(
+    name: 'Antigua and Barbuda',
+    flagPath: 'assets/flags/antigua_and_barbuda_flag.png',
+    capital: 'Saint John\'s',
+  ),
+  Country(
+    name: 'Bahamas',
+    flagPath: 'assets/flags/bahamas_flag.png',
+    capital: 'Nassau',
+  ),
+  Country(
+    name: 'Barbados',
+    flagPath: 'assets/flags/barbados_flag.png',
+    capital: 'Bridgetown',
+  ),
+  Country(
+    name: 'Belize',
+    flagPath: 'assets/flags/belize_flag.png',
+    capital: 'Belmopan',
+  ),
+  Country(
+    name: 'Canada',
+    flagPath: 'assets/flags/canada_flag.png',
+    capital: 'Ottawa',
+  ),
+  Country(
+    name: 'Costa Rica',
+    flagPath: 'assets/flags/costa_rica_flag.png',
+    capital: 'San Jose',
+  ),
+  Country(
+    name: 'Cuba',
+    flagPath: 'assets/flags/cuba_flag.png',
+    capital: 'Havana',
+  ),
+  Country(
+    name: 'Dominica',
+    flagPath: 'assets/flags/dominica_flag.png',
+    capital: 'Roseau',
+  ),
+  Country(
+    name: 'Dominican Republic',
+    flagPath: 'assets/flags/dominican_republic_flag.png',
+    capital: 'Santo Domingo',
+  ),
+  Country(
+    name: 'El Salvador',
+    flagPath: 'assets/flags/el_salvador_flag.png',
+    capital: 'San Salvador',
+  ),
+  Country(
+    name: 'Grenada',
+    flagPath: 'assets/flags/grenada_flag.png',
+    capital: 'Saint George\'s',
+  ),
+  Country(
+    name: 'Guatemala',
+    flagPath: 'assets/flags/guatemala_flag.png',
+    capital: 'Guatemala City',
+  ),
+  Country(
+    name: 'Haiti',
+    flagPath: 'assets/flags/haiti_flag.png',
+    capital: 'Port-au-Prince',
+  ),
+  Country(
+    name: 'Honduras',
+    flagPath: 'assets/flags/honduras_flag.png',
+    capital: 'Tegucigalpa',
+  ),
+  Country(
+    name: 'Jamaica',
+    flagPath: 'assets/flags/jamaica_flag.png',
+    capital: 'Kingston',
+  ),
+  Country(
+    name: 'Mexico',
+    flagPath: 'assets/flags/mexico_flag.png',
+    capital: 'Mexico City',
+  ),
+  Country(
+    name: 'Nicaragua',
+    flagPath: 'assets/flags/nicaragua_flag.png',
+    capital: 'Managua',
+  ),
+  Country(
+    name: 'Panama',
+    flagPath: 'assets/flags/panama_flag.png',
+    capital: 'Panama City',
+  ),
+  Country(
+    name: 'Saint Kitts and Nevis',
+    flagPath: 'assets/flags/saint_kitts_and_nevis_flag.png',
+    capital: 'Basseterre',
+  ),
+  Country(
+    name: 'Saint Lucia',
+    flagPath: 'assets/flags/saint_lucia_flag.png',
+    capital: 'Castries',
+  ),
+  Country(
+    name: 'Saint Vincent and the Grenadines',
+    flagPath: 'assets/flags/saint_vincent_and_the_grenadines_flag.png',
+    capital: 'Kingstown',
+  ),
+  Country(
+    name: 'Trinidad and Tobago',
+    flagPath: 'assets/flags/trinidad_and_tobago_flag.png',
+    capital: 'Port of Spain',
+  ),
+  Country(
+    name: 'United States of America',
+    flagPath: 'assets/flags/united_states_of_america_flag.png',
+    capital: 'Washington, D.C.',
+  ),
+];
---
+++ lib/service/flag_capitol_service.dart
@@ -0,0 +1,1 @@
+//functions that are associeted with flag_capitol model
